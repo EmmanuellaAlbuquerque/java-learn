67f76511f583e23b0db6e8b202fbd4f6e02f17fc
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2025-10-25 18:44:08 -0300

    java/security login logout custom form

---
+++ src/main/java/com/learn/java/infra/security/SecurityCustomForm.java
@@ -0,0 +1,48 @@
+package com.learn.java.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityCustomForm {
+
+    @Bean
+    public UserDetailsService registeredUsersInMemoryForm() {
+        UserDetails user1 = User.builder().username("manu").password("{noop}manu123").build();
+        UserDetails user2 = User.builder().username("vivi").password("{noop}vivi123").build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    /**
+     * Modifica login e logout padrão do Security
+     * @param http HttpSecurity
+     * @return SecurityFilterChain
+     * @throws Exception Throws by authorizeHttpRequests()
+     */
+    @Bean
+    public SecurityFilterChain securityFilter(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers("/css/**", "/js/**", "assets/**").permitAll();
+                    req.anyRequest().authenticated();
+                })
+                .formLogin(form -> form.loginPage("/login") // Login Filter
+                    .defaultSuccessUrl("/dashboard")
+                    .permitAll()
+                )
+                .logout(logout -> logout  // Logout Filter
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                )
+        .build();
+    }
+}
